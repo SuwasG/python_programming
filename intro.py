ed8a1752aa33f3649c61c9f44767ee8abcd7a157
SuwasG <suwasghale693@gmail.com>
2024-03-12 14:39:29 +0545

    intro to python

---
+++ intro.py
@@ -0,0 +1,18 @@
+'''
+-> Python is high-level, general-purpose, and very popular programming language.
+-> Python programming language is being used in WEB DEVELOPMENT, MACHINE LEARNING applications along with
+all cutting-edge technology in Software Industry.
+'''
+
+
+print('Hello Suwas!')
+print(f"this is f string       {1}")
+print(f'this is the  \n second line')
+
+print('Hello Sansar')
+
+# variable -> a container to store the value.
+# while writing the variable name: numeric value at the begining must be avoided. special characters except underscore (_) must be avoided. keywords and built in methods used in python must be avoided.
+
+num=22 
+print(num)
