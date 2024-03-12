b78a91dd16dbe59eb53957416d416358851e525c
SuwasG <suwasghale693@gmail.com>
2024-03-12 15:19:01 +0545

    comparision and logical operators in python

---
+++ comparision_and_logical_operator.py
@@ -0,0 +1,37 @@
+# comparison operator is used to compare two or more values.
+
+# equal to == 
+# not equal to != or <>
+# <, > , <= , >=
+
+
+x= 20
+y= 12
+
+print(x>y)
+print(x==y)
+print(x<y)
+print(x!=y)
+
+print(10==10)
+print(-10<-11)
+print(-10>-11)
+
+
+# Logical operator:
+# - used when compound condition is to be checked
+a=5
+b=7
+c=10
+
+# or -> Returns true if any one statement is true, otherwise false.
+# and -> Returns true if all statement are true, otherwise false.
+# not -> Inverse the result.
+
+print(a >b and a>c)
+print(a>c or a<b)
+print(c<a and b>a)
+print(a>a and b<b)
+
+print(not(a>b))
+
