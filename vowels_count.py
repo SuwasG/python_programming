32e1b5514c9418601beba4e8a1a3ba01cc71b82c
SuwasG <suwasghale693@gmail.com>
2024-03-15 17:54:25 +0545

    vowels count in python

--- vowels_count.py
+++ vowels_count.py
@@ -19,7 +19,6 @@ def count_vowels(string):
     vowels = set('aeiou')  # Using a set for vowels
     count = sum(1 for char in string if char in vowels)  
     # counts how many times a character in string passes the if char in vowels test. For each such character, a 1 is generated, and then all these 1s are added together by sum, resulting in the total count of vowels in the string.
-
     return count
 
 vowel_count = count_vowels(userInput)
