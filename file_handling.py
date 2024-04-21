06dae5625ab119c5d58ab35a183e2d347e4c6365
SuwasG <suwasghale693@gmail.com>
2024-04-21 06:46:01 +0545

    file handling in python

---
+++ file_handling.py
@@ -0,0 +1,22 @@
+f=open('demo.txt', 'r')
+# data=f.read() # reads entire line
+# print(data)
+# print(type(data))
+
+line1=f.readline() # reads one line at a time.
+print(line1)
+line2=f.readline()
+print(line2)
+line3=f.readline()
+print(line3)
+line4=f.readline()
+print(line4)
+line5=f.readline()
+print(line5)
+line6=f.readline()
+print(line6)
+line7=f.readline()
+print(line7)
+
+
+f.close()
